3ab5a3235a9df866eef7a4266bfcf5fec308ec81
Shivam Kumar <126045226+shivamsinger@users.noreply.github.com>
2023-11-17 12:08:00 +0000

    intro variable

---
+++ basics/02.js
@@ -0,0 +1,15 @@
+// intro variable
+
+// variable can store some information
+// we can use that information later
+// we can change that information later
+
+// declare variable 
+var firstName = "Shivam"
+
+// use a variable 
+console.log(firstName)
+
+// change value
+firstName = "Ankur"
+console.log(firstName)
