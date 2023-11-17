60c5794fc08cc82a120494cdd1e7e9a25840ee3b
Shivam Kumar <126045226+shivamsinger@users.noreply.github.com>
2023-11-17 14:12:36 +0000

    sring indexing

---
+++ basics/06.js
@@ -0,0 +1,11 @@
+// string indexing 
+
+let myName = "Shivam DIXAI"
+
+// s h i v a m
+// 0 1 2 3 4 5
+
+// console.log(myName[3])
+// console.log(myName.length)
+console.log(myName.length)
+console.log(myName[myName.length-2]);
