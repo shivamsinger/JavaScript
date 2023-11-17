ac1471465393bd97a0f00cf9729c4fc721a9c993
Shivam Kumar <126045226+shivamsinger@users.noreply.github.com>
2023-11-17 12:37:15 +0000

    Rules for variable

---
+++ basics/03.js
@@ -0,0 +1,45 @@
+// Rules for naming variable 
+
+// you cannot start wuth Number
+// Example--
+
+// var 1value  = 10
+// console.log(1value)...(invalid syntax)
+
+
+// var value1 = 10;
+// console.log(value1)...(valid syntax)
+
+//+++++++++++++++++++++++++++++++++++++++++
+
+// you can use only (underscore_) or (dollare$) symbal
+// Example--
+
+// let my$Name = "shivam"
+// console.log(my$Name)...(valid syntax)
+
+
+// let $myName = "shivam"
+// console.log($myName)...(valid syntax)
+
+// let _myName = "shivam"
+// console.log(_myName)...(valid syntax)
+
+// let my_Name = "shivam"
+// console.log(my_Name)...(valid syntax)
+
+//++++++++++++++++++++++++++++++++++++++++++++++
+
+// you cannot use space
+
+// let my Name =  "Shivam"
+// console.log(my Name)...(invalid syntax)
+
+//  let myName = "Shivam"
+//  console.log(myName)...(valid syntax)
+
+// convention
+//...start with samll leter and use comelacse 
+
+
+
